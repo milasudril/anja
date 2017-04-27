21f068c08ea0bcb5c8db45e365d375ed5fdb1a4c
Torbjörn Rathsman <milasudril@gmail.com>
2017-04-27 17:08:09 +0200

    Import wavetable

---
+++ sessiondata/wavetable.hpp
@@ -0,0 +1,15 @@
+//@	{"targets":[{"name":"wavetable.hpp","type":"include"}]}
+
+#ifndef ANJA_WAVETABLE_H
+#define ANJA_WAVETABLE_H
+
+#include "waveform.hpp"
+#include "arrayfixed.hpp"
+
+namespace Anja
+	{
+	class Wavetable:public ArrayFixed<Waveform,128>
+		{};
+	}
+
+#endif
