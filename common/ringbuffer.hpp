70d3af17a54e6d235f92c6734708e90d06331176
Torbjörn Rathsman <milasudril@gmail.com>
2017-05-29 19:56:31 +0200

    Modify to new API

--- common/ringbuffer.hpp
+++ common/ringbuffer.hpp
@@ -8,46 +8,43 @@
 namespace Anja
 	{
 	template<class T>
-	class RingBuffer:private ArraySimple<T>
+	class RingBuffer
 		{
 		public:
-			using ArraySimple<T>::length;
-
-			explicit RingBuffer(size_t N):ArraySimple<T>(N)
-				,m_offset_read(0),m_offset_write(0)
+			explicit RingBuffer(size_t N):
+				m_offset_read(0),m_offset_write(0),m_data(N)
 				{}
 
-			void push_back(const T& x)
+			void push_back(T x)
 				{
 				auto owr=m_offset_write;
-				ArraySimple<T>::m_data[owr]=x;
-				uint32_t N=ArraySimple<T>::m_N;
-				owr=(owr+1)%N;
-				m_offset_write=owr;
+				m_data[owr]=x;
+				uint32_t N=length();
+				m_offset_write=(owr+1)%N;
 				}
 
 			T pop_front()
 				{
 				auto ore=m_offset_read;
-				auto ret=ArraySimple<T>::m_data[ore];
-				uint32_t N=ArraySimple<T>::m_N;
+				auto ret=m_data[ore];
+				uint32_t N=length();
 				m_offset_read=(ore+1)%N;
 				return ret;
 				}
 
 			bool full() const
-				{
-				return m_offset_write < m_offset_read;
-				}
+				{return m_offset_write < m_offset_read;}
 
 			bool empty() const
-				{
-				return m_offset_read==m_offset_write;
-				}
+				{return m_offset_read==m_offset_write;}
+
+			uint32_t length() const noexcept
+				{return m_data.length();}
 
 		private:
 			volatile uint32_t m_offset_read;
 			volatile uint32_t m_offset_write;
+			ArraySimple<T> m_data;
 		};
 	}
 
