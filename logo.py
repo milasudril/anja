a6af57fdfdcd51a15088c5a02952eb16f020bd1c
Torbjörn Rathsman <milasudril@gmail.com>
2017-06-18 15:25:00 +0200

    Add memoized renderer for logo

--- logo.py
+++ logo.py
@@ -3,14 +3,17 @@
 #@      [
 #@           {
 #@			 "name":"logo.png","dependencies":
-#@				[{"ref":"logo.blend","rel":"file"},{"ref":"blender","rel":"tool"}]
+#@				[
+#@					 {"ref":"logo.blend","rel":"file"}
+#@					,{"ref":"memorender.py","rel":"file"}
+#@					,{"ref":"blender","rel":"tool"}
+#@				]
 #@			 }
 #@      ]
 #@  }
 
-import subprocess
 import sys
-import os
+import memorender
 
 def write_error(*args, **kwargs):
     print(*args,file=sys.stderr,**kwargs)
@@ -20,15 +23,7 @@ try:
 	in_dir=sys.argv[2];
 
 	print('# Baking logo')
-
-	blender=subprocess.Popen(['blender','-b',in_dir+'/logo.blend' \
-		,'-s','1','-e','1','-o',target_dir+'/'+in_dir+'/logo_#.png','-a'] \
-		,stdout=subprocess.PIPE)
-	for lines in blender.stdout:
-		progress=lines.decode('utf8').rstrip().split('|')
-		print('# Blender: %s'%(progress[-1].strip()))
-
-	os.rename(target_dir+'/'+in_dir+'/logo_1.png',target_dir+'/'+in_dir+'/logo.png')
+	memorender.render(target_dir,in_dir,'logo.png')
 
 	sys.exit(0)
 except Exception:
---
+++ memorender.py
@@ -0,0 +1,50 @@
+import sys
+
+def __render__(scene,target_dir,in_dir,target_name):
+	import bpy
+	scene.render.filepath=target_dir+'/'+in_dir+'/'+target_name
+	bpy.ops.render.render(write_still=True)
+	img=bpy.data.images.load(scene.render.filepath)
+	if 'render_result' in bpy.data.images:
+		bpy.data.images.remove(bpy.data.images['render_result'],do_unlink=True)
+	img.name='render_result'
+	img.use_fake_user=True
+	img.pack(as_png=True)
+	scene['up_to_date']=True
+	bpy.ops.wm.save_mainfile()
+
+def renderUpToDate(scene):
+	if scene.get('up_to_date',False):
+		return True
+	return False
+
+def main():
+	import bpy
+	argv=sys.argv
+	if "--" not in argv:
+		argv = []  # as if no args are passed
+	else:
+		argv = argv[argv.index("--") + 1:] # get all args after "--"
+
+	target_dir=argv[0]
+	in_dir=argv[1]
+	target=argv[2]
+
+	if renderUpToDate(bpy.context.scene):
+		bpy.data.images['render_result'].save_render(target_dir+'/'+in_dir+'/'+target)
+	else:
+		bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
+		bpy.context.user_preferences.addons['cycles'].preferences.devices[0].use = True
+		__render__(bpy.context.scene,target_dir,in_dir,target)
+
+if __name__ == "__main__":
+	main()
+
+def render(target_dir,in_dir,target_name):
+	import subprocess
+	blender=subprocess.Popen(['blender','-b',in_dir+'/logo.blend' \
+		,'--factory-startup','--python','memorender.py','--',target_dir,in_dir,target_name] \
+		,stdout=subprocess.PIPE)
+	for lines in blender.stdout:
+		progress=lines.decode('utf8').rstrip().split('|')
+		print('# Blender: %s'%(progress[-1].strip()))
