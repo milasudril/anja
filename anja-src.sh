31a8f685de3f5a3b1e84c5308de34cfbc6b3d5b3
Torbjörn Rathsman <milasudril@gmail.com>
2017-07-07 12:34:40 +0200

    Add source archive generator

--- anja-src.sh
+++ anja-src.sh
@@ -2,6 +2,14 @@
 #@	"targets":
 #@		[{
 #@		"name":"anja-src.tar.gz","dependencies":
-#@			[{"ref":"versioninfo.txt","rel":"misc"}]
+#		Track `projectinfo.hpp` instead of versioninfo, so this file is executed
+#		after `projectinfo.py`. Also notice that we will always regenerate the
+#		source archive, since `projectinfo.hpp` is dynamic.
+#@			[{"ref":"projectinfo.hpp","rel":"misc"}]
 #@		}]
 #@	}
+
+dir_target=$1
+find . | grep -v '^.$' | grep -v '__.*' | grep -v 'gh-pages' | grep -v '/\..*' \
+	| grep -v '.*\.blend1' \
+	| tar '--transform=s,^\.,anja-src,' -czf "$dir_target"/anja-src.tar.gz -T -
