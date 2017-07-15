01fb6480181ea5f23a945f56c16fca63431eeed2
Torbjörn Rathsman <milasudril@gmail.com>
2017-07-15 14:10:00 +0200

    Better command line options chapter

--- doc/cmdline.sh
+++ doc/cmdline.sh
@@ -13,9 +13,17 @@ in_dir="$2"
 set -e
 set -o pipefail
 
-__targets_dbg/anja --help \
+__targets_dbg/anja --help | csplit --prefix="$dir_target"/"$in_dir"/cmdline_ - '/^--*/-1'  > /dev/null
+csplit "$dir_target"/"$in_dir"/cmdline_01 --prefix="$dir_target"/"$in_dir"/cmdline_01_ '/^Common types$/' > /dev/null
+
+sed 's/\. For.*/. The different argument types accepted by the different options are/' "$dir_target"/"$in_dir"/cmdline_00 \
+	| (cat && tail -n +3 "$dir_target"/"$in_dir"/cmdline_01_01 \
+		| sed 's/^\([a-z][a-z ]*\) \:=/ * *\1* :=/') \
+	| cat - "$dir_target"/"$in_dir"/cmdline_01_00 \
 	| sed 's/\(^--[a-z][a-z\-]*\)\(\[\?=\)\([a-z ]*\)/ * `\1`\2*\3*/g' \
-	| markdown | (cat <<EOF
+	| sed 's/^    \(.*\)/\n \1/' \
+	| markdown \
+	| (cat <<EOF
 <content>
 EOF
 cat
