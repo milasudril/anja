2902d52600af704b135283c18c9cae349bbdbe88
Torbjörn Rathsman <milasudril@gmail.com>
2017-07-24 15:09:19 +0200

    Module interaction diagram

--- doc/modinteract.sh
+++ doc/modinteract.sh
@@ -1,10 +1,10 @@
 #@	{
 #@	"targets":
 #@		[{
-#@		"name":"signalflow.svg","dependencies":
+#@		"name":"modinteract.svg","dependencies":
 #@			[{"ref":"dot","rel":"tool"}
 #@			,{"ref":"xsltproc","rel":"tool"}
-#@			,{"ref":"signalflow.dot","rel":"misc"}
+#@			,{"ref":"modinteract.dot","rel":"misc"}
 #@			,{"ref":"dotsvgfilter.xsl","rel":"misc"}]
 #@		}]
 #@	}
@@ -19,7 +19,7 @@ set -eo pipefail
 target_dir="$1"
 in_dir="$2"
 
-dot -Tsvg "$in_dir"/signalflow.dot | xsltproc --novalid "$in_dir"/dotsvgfilter.xsl - \
-	> "$target_dir"/"$in_dir"/"signalflow.svg"
+dot -Tsvg "$in_dir"/playseq.dot | xsltproc --novalid "$in_dir"/dotsvgfilter.xsl - \
+	> "$target_dir"/"$in_dir"/"playseq.svg"
 
 trap : 0
