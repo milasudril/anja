8ae5ccabaeb5480d8b3929a03143da70fdf63078
Torbjörn Rathsman <milasudril@gmail.com>
2017-07-25 11:46:49 +0200

    Track versioninfo.txt instead of projectinfo.hpp

--- anja-src.sh
+++ anja-src.sh
@@ -2,11 +2,8 @@
 #@	"targets":
 #@		[{
 #@		"name":"anja-src.tar.gz","dependencies":
-#		Track `projectinfo.hpp`. This means that, in case `versioninfo.txt` is
-#		being updated we will get the new version. If we track `versioninfo.txt`
-#		instead, it may happen that this script will be executed before
-#		`projectinfo.py` because `versioninfo.txt` may already exist.
-#@			[{"ref":"projectinfo.hpp","rel":"misc"}
+#		Track `versioninfo.txt` so we run `projectinfo.py` before this script
+#@			[{"ref":"versioninfo.txt","rel":"misc"}
 #@			,{"ref":"find","rel":"tool"}
 #@			,{"ref":"grep","rel":"tool"}
 #@			,{"ref":"tar","rel":"tool"}]
