24cb2fd2fd40d9cb39115d534158ad8b60842521
Torbjörn Rathsman <milasudril@gmail.com>
2017-07-25 21:35:29 +0200

    Work on manual generator script.

--- doc/index.sh
+++ doc/index.sh
@@ -4,37 +4,36 @@
 #@			 {"ref":"makepage.py","rel":"misc"}
 #@			,{"ref":"format.css","rel":"include_extra"}
 #@			,{"ref":"color.css","rel":"include_extra"}
-#@			,{"ref":"../icons/message_warning.svg","rel":"include_extra"}
-#@			,{"ref":"../icons/message_info.svg","rel":"include_extra"}
-#@			,{"ref":"../icons/message_error.svg","rel":"include_extra"}
 #@			,{"ref":"xsltproc","rel":"tool"}
-#@			,{"ref":"rm","rel":"tool"}
+#@			,{"ref":"awk","rel":"tool"}
+#@			,{"ref":"tee","rel":"tool"}
 #@			,{"ref":"cp","rel":"tool"}
+#@			,{"ref":"grep","rel":"tool"}
+#@			,{"ref":"sort","rel":"tool"}
+#@			,{"ref":"uniq","rel":"tool"}
+#@			,{"ref":"xargs","rel":"tool"}
 #@		]
 #@	}
 
 set -eo pipefail
 
-dest=$1
-src=$2
-in_dir=$3
+dest="$1"
+src="$2"
+in_dir="$3"
 dest_dir=`dirname "$1"`
+target_dir=`echo "$dest_dir" | sed 's,\(.*\)/.*,\1,'`
 
 export LC_ALL=C.UTF-8 #PEP 538
 export LANG=C.UTF-8
 
+for k in "${@:4}"; do
+	echo "$k"
+done | tee >(grep -v '^__' | tee >(xargs cp -t "$dest_dir") \
+	| awk -v prefix="$target_dir" '{print prefix "/" $0}' ) \
+	| grep '^__' | sort | uniq > "$dest_dir"/archive.txt
+
+echo "$dest" >> "$dest_dir"/archive.txt
+
 stylesheets='{"stylesheets":["format.css","color.css"],"in_dir":"'"$in_dir"'"}'
 xsltproc --path "$dest_dir" "$in_dir"/inputstub.xsl "$src" \
 	| "$in_dir"/makepage.py "$stylesheets" > "$dest"
-
-if [[ -d gh-pages ]]; then #Test if we have cloned the gh-pages repo
-	rm -r gh-pages/*
-	for k in "${@:4}"
-	do
-		if [[ "${k:0:2}" != "__" ]]; then
-			cp "$k" "$dest_dir"
-		fi
-		cp "$k" gh-pages
-	done
-	cp "$dest" gh-pages
-fi
