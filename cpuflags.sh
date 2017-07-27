97e6659d7e24be36ee8e903d7f6e8d4a7c3b53be
Torbjörn Rathsman <milasudril@gmail.com>
2017-07-27 10:44:48 +0200

    Working on more portable binary files

---
+++ cpuflags.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+diff --changed-group-format='%<' --unchanged-group-format='' <(g++ -march=native -Q --help=target | grep enabled ) <(g++ -Q --help=target| grep enabled ) \
+	| sed 's/\[enabled\]//' | sed 's/.*-\([a-z0-9]*\)/,"\1"/'
