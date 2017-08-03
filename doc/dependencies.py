b62f7dc0bdaa6fe72fe5082058833cd481e94170
Torbjörn Rathsman <milasudril@gmail.com>
2017-08-03 19:20:01 +0200

    Add external resources to manual

--- doc/dependencies.py
+++ doc/dependencies.py
@@ -19,15 +19,19 @@ def write_error(*args, **kwargs):
 doc=string.Template('''<?xml version="1.0"?>
 <content>
 <chapter id="deps">Dependencies</chapter>
-<p>This chapter lists all external dependencies used within the Anja project. The items listed in <ref>libs</ref> are also needed to run Anja. On some distributions, library packages are split into a runtime package and a development package. In this case, only the runtime package is needed to run Anja, but the development package is needed to compile Anja.</p>
+<p>This chapter lists all external dependencies, as extracted by <prgname>maike</prgname>, used within the Anja project. The items listed in <ref>libs</ref> are also needed to run Anja. On some distributions, library packages are split into a runtime package and a development package. In this case, only the runtime package is needed to run Anja, but the development package is needed to compile Anja.</p>
 
 <section id="libs">Libraries</section>
 <p>The following list contains all libraries except for the C++ runtime library,  required by Anja.</p>
 <ul><li>$libs</li></ul>
 
 <section id="tools">Tools</section>
-<p>The following list contains all tools, required in order to compile Anja.</p>
+<p>The following list contains all tools, required to compile Anja.</p>
 <ul><li>$tools</li></ul>
+
+<section id="tools">Packages and resources</section>
+<p>The following list contains all external resources, required to compile Anja. This list includes static resources like fonts, and packages for scripting languages.</p>
+<ul><li>$resources</li></ul>
 
 </content>
 ''')
@@ -45,6 +49,7 @@ try:
 	subst=dict()
 	subst['tools']='</li>\n<li>'.join( sorted( deps['tools'] ) )
 	subst['libs']='</li>\n<li>'.join( sorted( deps['libraries'] ) )
+	subst['resources']='</li>\n<li>'.join( sorted( deps['resources'] ) )
 
 	with open(target_dir + '/' + in_dir + '/dependencies.xml','wb') as output:
 		output.write(doc.substitute(subst).encode('utf-8'))
